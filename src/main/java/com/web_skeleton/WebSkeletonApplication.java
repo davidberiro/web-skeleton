4f00e2f9c0ad859fdf78dea92e7b8540cca6936d
David <deebenchimol@gmail.com>
2018-03-03 13:31:22 +0200

    initial commit

---
+++ src/main/java/com/web_skeleton/WebSkeletonApplication.java
@@ -0,0 +1,12 @@
+package com.web_skeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSkeletonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/web_skeleton/WebSkeletonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.web_skeleton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebSkeletonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
